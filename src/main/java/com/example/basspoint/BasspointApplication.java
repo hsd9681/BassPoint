d351056141980b001a910c23274dd36f78772588
hsd9681 <hsd9681@naver.com>
2025-02-25 21:42:15 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/example/basspoint/BasspointApplication.java
@@ -0,0 +1,13 @@
+package com.example.basspoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasspointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasspointApplication.class, args);
+	}
+
+}
